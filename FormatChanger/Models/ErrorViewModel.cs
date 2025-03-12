e299257e98562a10697958e42b76c28ce43d5261
madvicspar <viktoria.akkuzina@yandex.ru>
2025-03-12 19:10:32 +0500

    Добавлен проект веб-приложения

---
+++ FormatChanger/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FormatChanger.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
