af0b77c3708a11891c602e3b831b1394ae213dda
madvicspar <viktoria.akkuzina@yandex.ru>
2025-05-06 02:05:44 +0500

    feat: интервал после таблиц

--- FormatChanger/Services/Strategies/TableCorrectionStrategy.cs
+++ FormatChanger/Services/Strategies/TableCorrectionStrategy.cs
@@ -1,6 +1,8 @@
-﻿using DocumentFormat.OpenXml.Packaging;
+﻿using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Packaging;
 using DocumentFormat.OpenXml.Wordprocessing;
 using FormatChanger.Models.FormattingModels;
+using FormatChanger.Models.Helpers;
 
 namespace FormatChanger.Services.Strategies
 {
@@ -13,19 +15,27 @@ namespace FormatChanger.Services.Strategies
         {
             // TODO: выравнивание по ширине окна и по содержимому
             var settings = GetSettings(template);
-            var tables = doc.MainDocumentPart?.Document?.Body?.Descendants<Table>().ToList();
-            if (tables == null)
+            var body = doc.MainDocumentPart?.Document?.Body;
+            var tables = body?.Descendants<Table>().ToList();
+            if (!tables.Any())
                 return;
+
+            var normalStyleId = doc.MainDocumentPart.StyleDefinitionsPart.Styles.Elements<Style>()
+                .FirstOrDefault(s => s.StyleName?.Val == ParagraphTypes.Normal.ToString())?.StyleId;
 
             foreach (var table in tables)
             {
                 ApplyTableProperties(table, settings);
+                if (settings.AfterSpacing != 0)
+                {
+                    TrySetSpacingAfterTable(body.Elements<OpenXmlElement>().ToList(), table, settings.AfterSpacing, normalStyleId);
+                }
             }
         }
 
         public void ApplyTableProperties(Table table, TableSettingsModel settings)
         {
-            // TODO: интервал до и после таблицы
+            // TODO: интервал до
             // TODO: в бд хранить все в dxa, реализовать перевод, в интерфейсе - пт и см
             var props = table.Elements<TableProperties>().FirstOrDefault();
 
@@ -48,6 +58,38 @@ namespace FormatChanger.Services.Strategies
 
             props.AppendChild(new TableCellVerticalAlignment { Val = TableVerticalAlignmentValues.Center });
             props.AppendChild(new Justification { Val = JustificationValues.Center });
+        }
+
+        /// <summary>
+        /// Устанавливает интервал перед следующим абзацем после таблицы,
+        /// если он имеет стиль "Обычный текст" (в том числе и списки).
+        /// </summary>
+        private void TrySetSpacingAfterTable(List<OpenXmlElement> bodyElements, Table table, float spacing, string normalStyleId)
+        {
+            var index = bodyElements.IndexOf(table);
+            if (index < 0 || index >= bodyElements.Count - 1)
+                return;
+
+            if (bodyElements[index + 1] is Paragraph nextParagraph)
+            {
+                var styleId = nextParagraph.ParagraphProperties?.ParagraphStyleId?.Val;
+                if (string.IsNullOrEmpty(styleId) || styleId == normalStyleId)
+                {
+                    SetSpacingAfterTable(nextParagraph, spacing);
+                }
+            }
+        }
+
+        private void SetSpacingAfterTable(Paragraph nextParagraph, float spaceAfter)
+        {
+            var props = nextParagraph.ParagraphProperties ?? new ParagraphProperties();
+            nextParagraph.ParagraphProperties ??= props;
+
+            var spacing = props.GetFirstChild<SpacingBetweenLines>() ?? new SpacingBetweenLines();
+            props.RemoveAllChildren<SpacingBetweenLines>();
+            props.Append(spacing);
+
+            spacing.Before = spaceAfter.ToString();
         }
 
         public List<string> CheckFormatting(Paragraph paragraph, FormattingTemplateModel template)
