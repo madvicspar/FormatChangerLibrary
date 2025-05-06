a5f8a9f37ea182b820f0cf12d5788138aeaa465c
madvicspar <viktoria.akkuzina@yandex.ru>
2025-05-06 10:26:37 +0500

    wip: определение маркера списка

--- FormatChanger/Services/ParagraphExtractor.cs
+++ FormatChanger/Services/ParagraphExtractor.cs
@@ -19,16 +19,19 @@ namespace FormatChanger.Services
                 .Select((p, i) => new ParagraphModel
                 {
                     Paragraph = p,
-                    Type = GetType(p, paragraphs, i, styles)
+                    Type = GetType(doc, p, paragraphs, i, styles)
                 })
                 .Where(p => !string.IsNullOrEmpty(p.Paragraph.InnerText) && !p.Paragraph.Ancestors<TableCell>().Any()).ToList();
         }
 
-        private string GetType(Paragraph p, List<Paragraph> all, int i, Styles styles)
+        private string GetType(WordprocessingDocument doc, Paragraph p, List<Paragraph> all, int i, Styles styles)
         {
             var styleId = p.ParagraphProperties?.ParagraphStyleId?.Val;
             var style = styles.Elements<Style>().FirstOrDefault(x => x.StyleId == styleId);
             var name = style?.StyleName?.Val?.ToString();
+
+            var markerType = GetListMarker(doc, p);
+            // УРОВНИ НИЖЕ ЧЕТВЕРТОГО ИДУТ КАК 3 УРОВЕНЬ
 
             return name switch
             {
@@ -37,8 +40,46 @@ namespace FormatChanger.Services
                 "heading 3" => ParagraphTypes.ThirdH.ToString(),
                 _ when i > 0 && IsImage(all[i - 1]) => ParagraphTypes.ImageCaption.ToString(),
                 _ when i < all.Count - 1 && IsTable(all[i + 1]) => ParagraphTypes.TableCaption.ToString(),
+                _ when markerType == "-" => ParagraphTypes.Dash.ToString(),
+                _ when markerType == "." => ParagraphTypes.Period.ToString(),
+                _ when markerType == ")" => ParagraphTypes.Bracket.ToString(),
                 _ => ParagraphTypes.Normal.ToString()
             };
+        }
+
+        private string GetListMarker(WordprocessingDocument doc, Paragraph p)
+        {
+            var numberingProperties = p.ParagraphProperties?.NumberingProperties;
+            try
+            {
+                var numberingId = numberingProperties.NumberingId?.Val;
+                var ilvl = numberingProperties.NumberingLevelReference?.Val?.Value;
+
+                var numberingPart = doc.MainDocumentPart.NumberingDefinitionsPart ?? doc.MainDocumentPart.AddNewPart<NumberingDefinitionsPart>();
+
+                var numInstance = numberingPart.Numbering.Elements<NumberingInstance>()
+                    .FirstOrDefault(n => n.NumberID.Value == numberingId);
+
+                var abstractNumId = numInstance.Elements<AbstractNumId>().FirstOrDefault()?.Val?.Value;
+
+                var abstractNum = numberingPart.Numbering.Elements<AbstractNum>()
+                    .FirstOrDefault(a => a.AbstractNumberId.Value == abstractNumId);
+
+                var level = abstractNum.Elements<Level>().FirstOrDefault(l => l.LevelIndex == ilvl);
+
+                var format = level.NumberingFormat?.Val;
+
+                if (format == NumberFormatValues.Bullet)
+                    return "-";
+                var lvlText = level.LevelText?.Val?.Value;
+                if (string.IsNullOrEmpty(lvlText))
+                    return string.Empty;
+                return lvlText.Contains(')') ? ")" : ".";
+            }
+            catch
+            {
+                return string.Empty;
+            }
         }
 
         private bool IsImage(Paragraph p) => p.Descendants<Drawing>().Any();
