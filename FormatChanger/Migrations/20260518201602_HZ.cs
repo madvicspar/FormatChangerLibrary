ee673bd39ca418589db85bf7d5a5047904a16ba9
madvicspar <viktoria.akkuzina@yandex.ru>
2026-05-19 01:17:35 +0500

    feat: обновление моделей

---
+++ FormatChanger/Migrations/20260518201602_HZ.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FormatChanger.Migrations
+{
+    /// <inheritdoc />
+    public partial class HZ : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "HeadingLevel",
+                table: "HeadingSettings",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldType: "bigint");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<long>(
+                name: "HeadingLevel",
+                table: "HeadingSettings",
+                type: "bigint",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer");
+        }
+    }
+}
