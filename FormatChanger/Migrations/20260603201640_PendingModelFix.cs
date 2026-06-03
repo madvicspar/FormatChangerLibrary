daea65e087ae0369a4caffc3d8e31ff0e1b4e00b
madvicspar <viktoria.akkuzina@yandex.ru>
2026-06-04 01:17:08 +0500

    wip

---
+++ FormatChanger/Migrations/20260603201640_PendingModelFix.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FormatChanger.Migrations
+{
+    /// <inheritdoc />
+    public partial class PendingModelFix : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "RuleWeightsJson",
+                table: "EvaluationSystems",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Title",
+                table: "EvaluationSystems",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RuleWeightsJson",
+                table: "EvaluationSystems");
+
+            migrationBuilder.DropColumn(
+                name: "Title",
+                table: "EvaluationSystems");
+        }
+    }
+}
